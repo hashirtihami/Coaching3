8d58381457c05c1b111e6268ae443f2c9b518780
tihaami <hashirtihamipk@gmail.com>
2018-08-14 23:42:57 +0500

    Revert "jquery removed"
    This reverts commit 963ef93e91179e0ecd6abdf282d9ae3489da734d.

--- templates/bottom.inc.php
+++ templates/bottom.inc.php
@@ -54,5 +54,10 @@
 
 
 
+<!-- JAVASCRIPTS -->
+
+<script src="layout/scripts/jquery.min.js"></script>
+<script src="layout/scripts/jquery.backtotop.js"></script>
+<script src="layout/scripts/jquery.mobilemenu.js"></script>
 </body>
 </html>
