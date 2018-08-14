5f6e609efeb93c28393329c5e36eee638a480c56
Minahil <minahil.0007@gmail.com>
2018-08-14 23:00:25 +0500

    Ho gaya

--- index.php
+++ index.php
@@ -28,9 +28,9 @@
 <div class="wrapper row3">
   <main class="hoc container clear"> 
     <div class="howtouse">
-      <p class="nospace font-xs">Let us help you.</p>
+      <p class="nospace font-xs">A brief tutorial on</p>
       <h4 class="font-x2 font-x3">How to use</h4>
-      <p>yahan kuch likhna hay </p>
+      <p>Simply click <a href="database.php">here</a> and enter your location to find your desired institute and <a href="form.php"> here</a> to join us and increase your revenue! </p>
 
     </div>
     <!-- / main body -->
@@ -40,15 +40,15 @@
 <div class="wrapper bgded overlay" style="background-image:url('images/demo/backgrounds/02.png');">
   <article class="hoc container"> 
     <div class="group btmspace-30">
-      <div class="fl_left" style="margin-right:20px;"><img src="images/demo/60x60.png" alt=""></div>
+      <div class="fl_left" style="margin-right:20px;"></div>
       <div class="fl_left">
-        <p class="nospace font-xs">Ultrices pellentesque sed quis</p>
-        <h3 class="heading">Rutrum nunc phasellus</h3>
+        <p class="nospace font-xs">Truly an </p>
+        <h3 class="heading">unforgettable experience</h3>
       </div>
     </div>
-    <blockquote>Sodales in at felis morbi et semper nibh vitae condimentum diam proin aliquam nisl quis laoreet finibus tellus risus tristique ligula vel efficitur mi leo sit amet arcu ut consequat tortor turpis elementum sodales mauris finibus sed curabitur justo tellus dignissim nec libero vel laoreet vulputate dolor in porta massa nisl quis condimentum eros sodales a cras ultricies.</blockquote>
-    <em class="block btmspace-50 font-xs">Name / Title</em>
-    <footer><a class="btn inverse" href="#">View More &raquo;</a></footer>
+    <blockquote>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</blockquote>
+    <em class="block btmspace-50 font-xs">Minahil Ovais / minahil@gmail.com</em>
+    <footer></footer>
   </article>
 </div>
 <div class="wrapper row3">
@@ -62,8 +62,8 @@
         <article class="excerpt"><a href="form.php"><img src="images/demo/320x220.png" alt=""></a>
           <div class="excerpttxt">
             <h6 class="heading font-x1">Deal Silver</h6>
-            <p>With the monthly package you can display your coaching centers information will be displayed for a month, after that it will automatically disappear till the next payment.</p>
-            <h6 style="color:#3C1363;">1000 rps per month only</h6>
+            <p>With the monthly package you can display your coaching center's information which will be displayed for a month, after that it will automatically disappear till the next payment.</p>
+            <h6 style="color:#3C1363;">1000RS per month</h6>
           </div>
         </article>
       </li>
@@ -72,8 +72,8 @@
           <div class="excerpttxt">
             <h6 class="heading font-x1">Deal Gold</h6>
 
-            <p>Nunc non erat molestie faucibus duis aliquam rutrum sapien non pretium tortor lacinia auctor donec et ullamcorper lacus&hellip;</p>
-            <h6 style="color:#3C1363;">5000 rps for 6 months only</h6>
+            <p>With the six months package you can display your coaching center's information which will be displayed for 6 months, after that it will automatically disappear till the next payment.&hellip;</p>
+            <h6 style="color:#3C1363;">5000RS for 6 months </h6>
           </div>
         </article>
       </li>
@@ -82,8 +82,8 @@
           <div class="excerpttxt">
             <h6 class="heading font-x1">Deal Platinum</h6>
 
-            <p>Natoque penatibus et magnis dis parturient montes mus praesent sed massa nisi nunc tincidunt id sapien at maximus curabitur&hellip;</p>
-            <h6 style="color:#3C1363;">10,000 rps per year</h6>
+            <p>With the yearly package you can display your coaching center's information which will be displayed for a year, after that it will automatically disappear till the next payment.&hellip;</p>
+            <h6 style="color:#3C1363;">10,000RS per year</h6>
           </div>
         </article>
       </li>
@@ -92,19 +92,12 @@
 </div>
 <div class="wrapper bgded overlay" style="background-image:url('images/demo/backgrounds/03.png');">
   <article class="hoc container center"> 
-    <h3 class="font-x3">Sit consectetur adipiscing</h3>
-    <p class="btmspace-50">Elit aliquam scelerisque consequat augue vitae pellentesque mauris dapibus nec</p>
+    <h3 class="font-x3">Build your future</h3>
+    <p class="btmspace-50">By starting right away!</p>
     <footer><a class="inherit" href="#"><i class="fa fa-play-circle fa-4x"></i></a></footer>
   </article>
 </div>
-<div class="wrapper row3">
-  <section class="hoc container clear"> 
-    <div class="three_quarter first">
-      <h3 class="nospace">Velit sagittis magna hendrerit</h3>
-      <p class="nospace">Sit amet facilisis mi faucibus proin imperdiet nunc sed ligula dapibus bibendum</p>
-    </div>
-    <footer class="one_quarter"><a class="btn" href="#">More Information &raquo;</a></footer>
-  </section>
+
 </div>
 <!-- Footer Background Image Wrapper -->
 <div class="bgded overlay" style="background-image:url('images/demo/backgrounds/04.png');"> 
